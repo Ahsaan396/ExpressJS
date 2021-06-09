2a20926ba3f918cb0c9978b693fb3d5121dc3d19
Ahsanul Hakim <ahsanulhakim285@gmail.com>
2021-06-09 12:28:22 +0600

    first Application

---
+++ PracticeApp/index.js
@@ -0,0 +1,10 @@
+var express=require('express');
+app=express();
+
+app.get("/",function(req,res){
+    res.send("Hello Express Js");
+})
+
+app.listen(8000,function(){
+    console.log("Server Run Successfully")
+})
